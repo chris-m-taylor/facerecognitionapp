72f92fcc42b55f8c1fdf85766231feb30a87058f
Chris Taylor <chrismtaylor1998@gmail.com>
2020-09-03 22:15:53 -0400

    added tachyons and nav componenet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
       
     </div>
   );
